4de06ffcee5cd65026941cd8d041617a55e6687d
dirgepye <s.stenhouse87@gmail.com>
2015-11-13 19:39:28 +0000

    find numbered character in string
    function numberedString (string,number) {
    	return string[number];
    };
    
    AND
    
    function numberedStringUser (string,number) {
        return string[number -1]# Please enter the commit message for your changes. Lines starting
    };

--- answers.js
+++ answers.js
@@ -12,7 +12,7 @@ console.log(firstOfString("Montreal"));
 console.log(firstOfString(""));
 
 
-// this function takes an inputted string and uses the strings length as a position
+// the function below takes an inputted string and uses the strings length as a position
 // within the string to identify the a character we want to target. however, seeing
 // as javascript starts counting at [0], the length will always be one longer than
 // what is actually there so you have to subtract 1 to get a number that actually 
@@ -20,10 +20,30 @@ console.log(firstOfString(""));
 
 function lastOfString(lastString) {
     return lastString[lastString.length -1];
-}
+};
 
 console.log(lastOfString("Montreal"));
 console.log(lastOfString("Hellow World"));
 console.log(lastOfString(""));
 console.log(lastOfString("P"));
 
+//the function below takes a string and a number as input. the number entered
+//helps to identify a specific character and returns it. I also included a
+//more "user friendly" that would allow someone who isn't familiar with javascript
+//to more accurately identify the number they want to select
+
+function numberedString (string,number) {
+    return string[number];
+};
+
+console.log(numberedString("jerk store",2));
+console.log(numberedString("montreal",4));
+console.log(numberedString("",6));
+
+function numberedStringUser (string,number) {
+    return string[number -1]
+}
+
+console.log(numberedStringUser("jerk store",2));
+console.log(numberedStringUser("montreal",4));
+console.log(numberedStringUser("",6));
