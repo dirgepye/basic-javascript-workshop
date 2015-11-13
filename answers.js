90900d109f113ff32a19a5fd790828cb502fb026
dirgepye <s.stenhouse87@gmail.com>
2015-11-13 19:05:14 +0000

    Finding the first character in a string
    function firstOfString(string) {
        return string[0];# Please enter the commit message for your changes. Lines starting
    };# with '#' will be ignored, and an empty message aborts the commit.

--- answers.js
+++ answers.js
@@ -1,1 +1,12 @@
-console.log("Hellow earl!")
+console.log("Hellow earl!");
+
+//this string should return the first character in a string
+
+function firstOfString(string) {
+    return string[0];
+};
+
+console.log(firstOfString("Hello world"));
+console.log(firstOfString("Montreal"));
+console.log(firstOfString(""));
+
